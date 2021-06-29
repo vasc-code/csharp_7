f17cd4be3b3e190ac7c58451b7184da51fb6d4d0
Rafael Vasconcelos <rafael.vasconcelos@itenabler.com.br>
2021-06-29 09:38:43 -0300

    arquivos do curso de C# parte 7

---
+++ csharp_7.1/01/01.01/antes/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace _01_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Campainha campainha = new Campainha();
+                campainha.OnCompanhiaTocou += CampanhiaTocou1;
+                campainha.OnCompanhiaTocou += CampanhiaTocou2;
+                Console.WriteLine("A campanhia será tocada.");
+                campainha.Tocar("101");
+
+                campainha.OnCompanhiaTocou -= CampanhiaTocou1;
+                Console.WriteLine("Será tocada de novo.");
+                campainha.Tocar("102");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+
+            //campainha.OnCompanhiaTocou(null, new EventArgs());
+
+
+
+
+            Console.ReadKey();
+        }
+
+        static void CampanhiaTocou1(object sender, CampanhiaEventArgs args)
+        {
+            Console.WriteLine("A campanhia tocou no AP "+args.Apartamento+" (1)");
+            throw new Exception("Ocorreu um erro em campanhiaTocou1");
+        }
+        static void CampanhiaTocou2(object sender, CampanhiaEventArgs args)
+        {
+            Console.WriteLine("A campanhia tocou no AP " +args.Apartamento+" (2)");
+            throw new Exception("Ocorreu um erro em campanhiaTocou2");
+        }
+    }
+
+    class Campainha
+    {
+        public event EventHandler<CampanhiaEventArgs> OnCompanhiaTocou;
+        public void Tocar(string apartamento)
+        {
+            foreach (var manipulador in OnCompanhiaTocou.GetInvocationList())
+            {
+                try
+                {
+                    manipulador.DynamicInvoke(this, new CampanhiaEventArgs(apartamento));
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+            
+            
+            
+        }
+    }
+
+    class CampanhiaEventArgs: EventArgs
+    {
+        public CampanhiaEventArgs(string apartamento)
+        {
+            Apartamento = apartamento;
+        }
+
+        public string Apartamento { get; }
+
+    }
+}
---
+++ csharp_7.1/01/01.03/antes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _01_03
+{
+    class Program
+    {
+        delegate int Operacao(int a, int b);
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 2;
+
+            var operacao = new Operacao(Somar);
+
+            Console.WriteLine(operacao(a,b));
+
+            operacao = Subtrair;
+            Console.WriteLine(operacao(a,b));
+;
+            Console.WriteLine();
+
+            Console.ReadKey();
+
+        }
+
+        static int Somar(int a, int b)
+        {
+            Console.WriteLine($"A operação Somar foi chamada com a = {a} e {b}");
+            return a + b;
+        }
+
+        static int Subtrair(int a, int b)
+        {
+            Console.WriteLine($"A operação Subtrair foi chamada com a = {a} e {b}");
+            return a - b;
+        }
+    }
+}
